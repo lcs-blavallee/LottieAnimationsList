ac0501c8625ca9ba0c0b145853d51b5a36bb670b
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-02-02 09:19:08 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Benjamin Lavallee on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
